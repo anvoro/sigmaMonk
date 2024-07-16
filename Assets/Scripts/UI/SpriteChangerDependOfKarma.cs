73f7fcfccfe117b404a11e69e39116a7c6bf690d
Anton Vorobev <anonspirov@yandex.ru>
2024-07-16 22:05:53 +0300

    wip

---
+++ Assets/Scripts/UI/SpriteChangerDependOfKarma.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class SpriteChangerDependOfKarma : MonoBehaviour
+	{
+		[Serializable]
+		private struct KarmaImageItem
+		{
+			public Sprite Sprite;
+			public int KarmaToShow;
+		}
+
+		private SpriteRenderer _sprite;
+
+		[SerializeField] private KarmaImageItem[] _items;
+
+		private void Awake()
+		{
+			_sprite = GetComponent<SpriteRenderer>();
+		}
+
+		private void Start()
+		{
+			GameManager.OnKarmaChange += OnKarmaChange;
+		}
+
+		private void OnDestroy()
+		{
+			GameManager.OnKarmaChange -= OnKarmaChange;
+		}
+
+		private void OnKarmaChange(int value, int delta)
+		{
+			foreach (var item in _items)
+			{
+				if (item.KarmaToShow == value)
+				{
+					Debug.Log(value);
+					
+					_sprite.sprite = item.Sprite;
+					break;
+				}
+			}
+		}
+	}
+}
